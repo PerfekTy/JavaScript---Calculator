b3b6d32cb409fad760dc2502f45198955b3c57d5
Sebastian Feleńczak <sebastianfelenczak@Sebastians-MacBook-Air.local>
2022-10-19 17:43:11 +0200

    writing js

---
+++ main.js
@@ -0,0 +1,13 @@
+const numbers = document.querySelectorAll('.number-btn'),
+ operator = document.querySelectorAll('.operator'),
+ equal = document.querySelector('.equals'),
+ prevoiusNumber = docuemnt.querySelector('.prevoiusNumber'),
+ currentNumber = document.querySelector('.currentNumber'),
+ clear = document.querySelector('.clear'),
+ del = document.querySelector('.del');
+
+let value = '';
+
+numbers.addEventListener('click', ()=> {
+    console.log('dziala');
+})
