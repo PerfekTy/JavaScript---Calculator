816b88980cb5c3489eeee4a0349d5b00f6a86a7f
Sebastian Felenczak <sebastianfelenczak@gmail.com>
2022-10-20 09:03:13 +0200

    still working

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-const numbers = document.querySelectorAll('.number-btn'),
- operator = document.querySelectorAll('.operator'),
- equal = document.querySelector('.equals'),
- prevoiusNumber = docuemnt.querySelector('.prevoiusNumber'),
- currentNumber = document.querySelector('.currentNumber'),
- clear = document.querySelector('.clear'),
- del = document.querySelector('.del');
+const numbers = document.querySelectorAll(".number-btn"),
+  operator = document.querySelectorAll(".operator"),
+  equal = document.querySelector(".equals"),
+  previousNumber = document.querySelector(".previousNumber"),
+  currentNumber = document.querySelector(".currentNumber"),
+  clear = document.querySelector(".clear"),
+  del = document.querySelector(".del"),
+  mathSign = document.querySelector(".mathSign");
 
-let value = '';
+let value = "";
 
-numbers.addEventListener('click', ()=> {
-    console.log('dziala');
-})
+function addNumber() {
+  if (this.textContent === "," && currentNumber.innerHTML.includes(",")) return;
+  if (this.textContent === "," && currentNumber.innerHTML.includes(""))
+    return (currentNumber.innerHTML = "0,");
+  currentNumber.innerHTML += this.textContent;
+}
+
+function operate() {
+  if (currentNumber.innerHTML === "" && this.textContent === "-") {
+    currentNumber.innerHTML = "-";
+    return;
+  } else if (currentNumber.innerHTML === "") {
+    return;
+  }
+  previousNumber.innerHTML = currentNumber.innerHTML;
+  mathSign.innerHTML = this.textContent;
+  currentNumber.innerHTML = "";
+}
+
+numbers.forEach(btn => {
+  btn.addEventListener("click", addNumber);
+});
+
+operator.forEach(e => {
+  e.addEventListener("click", operate);
+});
