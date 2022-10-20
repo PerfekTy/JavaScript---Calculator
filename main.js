56c7c229b61c5ab7fd2cfb136865974db92eaa79
Sebastian Felenczak <sebastianfelenczak@gmail.com>
2022-10-20 10:21:51 +0200

    working calulator

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const numbers = document.querySelectorAll(".number-btn"),
+const numbers = document.querySelectorAll(".number"),
   operator = document.querySelectorAll(".operator"),
   equal = document.querySelector(".equals"),
   previousNumber = document.querySelector(".previousNumber"),
   currentNumber = document.querySelector(".currentNumber"),
   clear = document.querySelector(".clear"),
   del = document.querySelector(".del"),
-  mathSign = document.querySelector(".mathSign");
+  mathSign = document.querySelector(".mathSign"),
+  pow = document.querySelector(".pow");
 
-let value = "";
+let result = "";
 
 function addNumber() {
   if (this.textContent === "," && currentNumber.innerHTML.includes(",")) return;
@@ -23,9 +24,49 @@ function operate() {
   } else if (currentNumber.innerHTML === "") {
     return;
   }
+
+  if (mathSign.innerHTML !== "") {
+    showResult();
+  }
+
   previousNumber.innerHTML = currentNumber.innerHTML;
-  mathSign.innerHTML = this.textContent;
+  mathSign.innerHTML += this.textContent;
   currentNumber.innerHTML = "";
+}
+
+function showResult() {
+  if (previousNumber.innerHTML === "" || currentNumber.innerHTML === "") return;
+  let a = Number(currentNumber.innerHTML);
+  let b = Number(previousNumber.innerHTML);
+  let operator = mathSign.innerHTML;
+
+  switch (operator) {
+    case "+":
+      result = a + b;
+      break;
+
+    case "-":
+      result = b - a;
+      break;
+
+    case "×":
+      result = a * b;
+      break;
+
+    case "÷":
+      result = b / a;
+      break;
+  }
+
+  currentNumber.innerHTML = result;
+  previousNumber.innerHTML = "";
+  mathSign.innerHTML = "";
+}
+
+function clearEverything() {
+  currentNumber.innerHTML = "";
+  previousNumber.innerHTML = "";
+  mathSign.innerHTML = "";
 }
 
 numbers.forEach(btn => {
@@ -35,3 +76,7 @@ numbers.forEach(btn => {
 operator.forEach(e => {
   e.addEventListener("click", operate);
 });
+
+equal.addEventListener("click", showResult);
+
+clear.addEventListener("click", clearEverything);
